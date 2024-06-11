c46222728166cdc570545930162857ab6fa75240
Zebby <48347420+TwilightZebby@users.noreply.github.com>
2024-06-11 10:08:42 +0100

    Update en-GB.js

--- Locales/en-GB.js
+++ Locales/en-GB.js
@@ -109,7 +109,7 @@ module.exports = {
 
 
     // ******* FOR SUPPORT COMMAND
-    SUPPORT_COMMAND_RESPONSE: `Need help with using HomeCord? Want to report an issue or suggest a new idea for HomeCord?\nFeel free to join HomeCord's Support Server ("Twilight Domain") or view its Documentation via the links below!`,
+    SUPPORT_COMMAND_RESPONSE: `Need help with using HomeCord? Want to report an issue or suggest a new idea for HomeCord?\nFeel free to join HomeCord's Support Server or view its Documentation via the links below!`,
     SUPPORT_COMMAND_BUTTON_LABEL: `Join Support Server`,
     SUPPORT_COMMAND_DOCS_BUTTON_LABEL: `View Documentation`,
 
