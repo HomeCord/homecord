a570130472b13025730cda32ecda01ea75acc0ab
Zebby <48347420+TwilightZebby@users.noreply.github.com>
2024-07-24 10:55:41 +0100

    Swap out HomeCord's Emojis (in text content) for App Emojis instead of Server Emojis

---
+++ Resources/appEmojis.js
@@ -0,0 +1,11 @@
+
+// Only add App Custom Emojis to this, not Server Custom Emojis
+
+module.exports = {
+
+    EMOJI_SCHEDULED_EVENTS: "<:scheduledEvents:1265040099226423401>",
+    EMOJI_CHANNEL_THREAD: "<:channelThread:1265040034600452189>",
+    EMOJI_CHANNEL_FORUM: "<:channelForum:1265039968200560711>",
+    EMOJI_BLURPLE_SPARKLES: "<:blurpleSparkles:1265039890114941082>"
+
+}
